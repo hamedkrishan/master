dbad7fa8792cca91981807907630ca251e7128a3
github_hamedkrishan_here <hamedkrishan676@gamil.com>
2025-04-06 10:49:49 +0300

    first commoit

---
+++ resources/views/layouts/header-appointment.blade.php
@@ -0,0 +1,12 @@
+<div class="container-fluid page-header py-5 mb-5 wow fadeIn" data-wow-delay="0.1s">
+        <div class="container text-center py-5 mt-4">
+            <h1 class="display-2 text-white mb-3 animated slideInDown">Appoinment</h1>
+            <nav aria-label="breadcrumb animated slideInDown">
+                <ol class="breadcrumb justify-content-center mb-0">
+                    <li class="breadcrumb-item"><a href="#">Home</a></li>
+                    <li class="breadcrumb-item"><a href="#">Pages</a></li>
+                    <li class="breadcrumb-item" aria-current="page">Appoinment</li>
+                </ol>
+            </nav>
+        </div>
+    </div>
