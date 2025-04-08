9f631b59b99002e9c34687e54fe9f43dccda3cce
github_hamedkrishan_here <hamedkrishan676@gamil.com>
2025-04-09 01:54:07 +0300

    Fix bugs and update home page layout

--- resources/views/layouts/bottom.blade.php
+++ resources/views/layouts/bottom.blade.php
@@ -1,3 +1,7 @@
+ 
+ 
+ 
+ 
  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
     <script src="lib/wow/wow.min.js"></script>
@@ -11,3 +15,4 @@
 </body>
 
 </html>
+
