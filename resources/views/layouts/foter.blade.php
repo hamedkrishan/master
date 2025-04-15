cecd4b7f7fd80cb30606830c1207f8548d059081
github_hamedkrishan_here <hamedkrishan676@gamil.com>
2025-04-15 09:54:59 +0300

    update views files and fix line endings

--- resources/views/layouts/foter.blade.php
+++ resources/views/layouts/foter.blade.php
@@ -1,4 +1,9 @@
-  <div class="container-fluid footer position-relative bg-dark text-white-50 py-5 wow fadeIn" data-wow-delay="0.1s">
+ 
+ 
+ 
+ 
+ 
+ <div class=""></div> <div class="container-fluid footer position-relative bg-dark text-white-50 py-5 wow fadeIn" data-wow-delay="0.1s">
         <div class="container">
             <div class="row g-5 py-5">
                 <div class="col-lg-6 pe-lg-5">
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('components.home.home');
+    return view('OurTeam.ourteam');
 });
